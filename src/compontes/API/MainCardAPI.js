297a7ed9307402b203050290e4e0faee9ae1e014
magaramol09 <magaramol09@gmail.com>
2022-04-11 17:05:26 +0530

    Main&Main-card C

---
+++ src/compontes/API/MainCardAPI.js
@@ -0,0 +1,41 @@
+
+const MainCardAPI = [
+    {
+      id: 1,
+      img: "./img/house-1.jpg ",
+      title: "Meta Trap house",
+      info: "@freexecorle...0x89acc..",
+    },
+    {
+      id: 2,
+      img: "./img/house-1.jpg ",
+      title: "Meta Trap house",
+      info: "@freexecorle...0x89acc..",
+    },
+    {
+      id: 3,
+      img: "./img/house-1.jpg ",
+      title: "Meta Trap house",
+      info: "@freexecorle...0x89acc..",
+    },
+    {
+      id: 4,
+      img: "./img/house-1.jpg ",
+      title: "Meta Trap house",
+      info: "@freexecorle...0x89acc..",
+    },
+    {
+      id: 5,
+      img: "./img/house-1.jpg ",
+      title: "Meta Trap house",
+      info: "@freexecorle...0x89acc..",
+    },
+    {
+      id: 6,
+      img: "./img/house-1.jpg ",
+      title: "Meta Trap house",
+      info: "@freexecorle...0x89acc..",
+    },
+]
+
+export default MainCardAPI;
---
+++ src/compontes/Main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Main() {
+    return (<>
+        <div className='container main-section'>
+            <h3>Live Spaces</h3>
+            <div className="form-check checkbox-lg">
+                <input className="form-check-input rounded-pill" type="checkbox" value="" id="flexCheckChecked" checked />
+                <label className="form-check-label" for="flexCheckChecked">
+                    All NFTs on Cyber eithr belong t or were minted byt their space creator.
+                </label>
+            </div>
+            <div className="row main-routing">
+
+                <div className="col-4 col-lg-2 main-routing-heading">
+                    <i class="fa-solid fa-fire-flame-curved" i />
+                    <span className="main-routing-heading-item"> <a href=""> 24h Trending </a></span>
+                </div>
+                <div className="col-4 col-lg-2 main-routing-heading">
+                    <i class="fa-solid fa-chart-mixed"></i>
+                    <span className="main-routing-heading-item"><a href=""> letest show </a></span>
+                </div>
+                <div className="col-4 col-lg-2 main-routing-heading">
+                    <i class="fa-solid fa-fire-flame-curved" i />
+                    <span className="main-routing-heading-item"><a href=""> most popular </a></span>
+                </div>
+                <div className="col-4 col-lg-2 main-routing-heading">
+                    <i class="fa-solid fa-fire-flame-curved" i />
+                    <span className="main-routing-heading-item"><a href=""> in denesis </a></span>
+                </div>
+                <div className="col-4 col-lg-2 main-routing-heading">
+                    <i class="fa-solid fa-fire-flame-curved" i />
+                    <span className="main-routing-heading-item"><a href=""> in temple </a></span>
+                </div>
+                <div className="col-4 col-lg-2 main-routing-heading">
+                    <i class="fa-solid fa-fire-flame-curved" i />
+                    <span className="main-routing-heading-item"><a href=""> in void </a></span>
+                </div>
+            </div>
+        </div>
+    </>
+    )
+}
